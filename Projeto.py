5891e939c9be5c8d9d8a74a770a0b064ce7135c6
Carlos Santos <carlosvit26@gmail.com>
2023-09-22 20:34:03 -0300

    mov_carlimm

--- Projeto.py
+++ Projeto.py
@@ -1,16 +1,62 @@
+from typing import Any
+from pygame.locals import *
 import pygame
 import sys
+import os
 
 class Jogador(pygame.sprite.Sprite):
     def __init__(self, x: int, y: int):
         pygame.sprite.Sprite.__init__(self) # Inicia a classe pai sprite
-        self.image = pygame.image.load("playerPH.png") #Carrega a image do jogador
-        self.image = pygame.transform.scale(self.image, (25, 50)) #Diminui o tamanho da image
-        self.pos = pygame.Vector2(x, y) #Define a posição como objeto Vector2 (Vetor bidimensional)
-        self.rect = self.image.get_rect(center = (self.pos.x, self.pos.y)) #Define o objeto rect tendo centro no centro da image
+    
+        #armazenar as sprites em uma lista
+        self.sprites = []
 
+        #Chamdando a pasta onde estão as imagens do personagem
+        self.pasta = './personagem/'
+
+        #Acessando pasta
+        self.itens_na_pasta = os.listdir(self.pasta)
+        
+        for item in self.itens_na_pasta:
+            for i in range(1,len(item)):
+                #Chamando cada imagem e adicionando na lista self.sprites
+                self.sprites.append(pygame.image.load(f'personagem/run-{i}.png'))
+
+        #Primeira imagem que aparece na tela
+        self.atual = 0
+        self.image = self.sprites[self.atual] 
+
+        #Define a posição como objeto Vector2 (Vetor bidimensional)
+        self.pos = pygame.Vector2(x, y)
+        self.rect = self.image.get_rect(center = (self.pos.x, self.pos.y)) #Define o objeto rect tendo centro no centro da image
         self.tamanho = self.rect.width #Define tamanho como largura do rect
-        self.vel = 1 #Define velocidaded
+        self.vel = 1 #Define velocidade
+
+
+        self.animar = False
+
+    #Fazer o personagem andar
+    def andar(self):
+        self.animar = True
+
+    #Fazer o personagem parar
+    def parar(self):
+        self.animar = False
+
+    #Animação do personagem
+    def update(self):
+        if self.animar == True:
+            #Mudança de imagem na iterações
+            self.atual = self.atual + 0.2
+            
+            #Voltando para o 0, após terminar as sprites
+            if self.atual>=len(self.sprites):
+                self.atual = 0
+                #self.animar = False #Precisa mudar
+            self.image = self.sprites[int(self.atual)]
+            #self.image = pygame.transform.scale(self.image, (128*3,64*3))
+
+
 
     def move(self, direcao):
         #Checks para que o jogador não saia do limite da tela
@@ -50,7 +96,9 @@ x_circulo = largura // 2
 y_circulo = altura // 2
 
 #Define objeto Jogador
-jogador = Jogador(largura / 2, altura / 2)
+todas_sprites = pygame.sprite.Group()
+jogador = Jogador(largura / 2, altura /2)
+todas_sprites.add(jogador)
 grupoJogador = pygame.sprite.GroupSingle()
 grupoJogador.add(jogador)
 
@@ -61,6 +109,17 @@ while True:
         if evento.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LSHIFT]:
+            jogador.andar()
+        else:
+            jogador.parar()
+
+    #Desenhando meu jogador        
+    todas_sprites.draw(janela)
+    todas_sprites.update()
+
 
     #Recebe e lida com inputs do teclado
     keys = pygame.key.get_pressed()

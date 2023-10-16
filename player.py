ac58c9666ad973c6ecd2afd46e47fd4e5fd84a96
pedroLucabral <pedro.simoes@academico.ufpb.br>
2023-10-16 18:06:50 -0300

    player

--- player.py
+++ player.py
@@ -7,7 +7,13 @@ class Player(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.image.load("heroi/personagem0.png")
-        self.pos = pygame.Vector2(x, y)
-        self.rect = self.image.get_rect(center= (self.pos.x, self.pos.y))
+        self.pos = pygame.math.Vector2(x, y)
+        self.rect = self.image.get_rect(center = (self.pos.x, self.pos.y))
 
-    
+        self.direction = pygame.math.Vector2
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.direction.y = -1

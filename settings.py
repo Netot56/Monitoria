c0b620210253fc62d64f300a499e4e52e280cf8d
Carlos Santos <carlosvit26@gmail.com>
2023-10-16 19:09:54 -0300

    animação

--- settings.py
+++ settings.py
@@ -1,6 +1,18 @@
 import pygame
 import fonts
 
+def import_folder(path):
+	surface_list = []
+
+	for _,__,img_files in walk(path):
+		for image in img_files:
+			full_path = path + '/' + image
+			image_surf = pygame.image.load(full_path).convert_alpha()
+			surface_list.append(image_surf)
+
+	return surface_list
+
+
 width = 1280
 height = 720
 fps = 30
